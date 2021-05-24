80e49dc5c4b8234c97a0ff9775a2f720ddddbb64
gongyuh <2736498007@qq.com>
2021-05-24 21:46:38 +0800

    用户登录返回信息

--- src/api/loginController.js
+++ src/api/loginController.js
@@ -2,8 +2,8 @@ import send from "../config/MailConfig";
 import moment from "dayjs";
 import jsonwebtoken from "jsonwebtoken";
 import config from "../config/index";
-import { checkCode } from '../common/utils'
-import User from '../model/User'
+import { checkCode } from "../common/utils";
+import User from "../model/User";
 
 class loginController {
   constructor() {}
@@ -30,46 +30,51 @@ class loginController {
   async login(ctx) {
     //   debugger
     //   // 接收用户的数据
-      // 返回token
-      const { body } = ctx.request
-      let sid = body.sid
-      let code = body.code
-      // 验证图片验证码的时效性、正确性
-      // let result = await checkCode(sid, code)
-      let result = true
-      if (result) {
-        //验证用户账号密码是否正确
-        let checkUserPasswd = false
-        let user = await User.findOne({ username: body.username })
-        if (user.password === body.password) {
-          checkUserPasswd = true
-        }
-        // mongoDB查库
-        if (checkUserPasswd) {
-          // 验证通过，返回Token数据
-          console.log('Hello login')
-          let token = jsonwebtoken.sign({ _id: 'brian' }, config.JWT_SECRET, {
-            expiresIn: '1d'
-          })
-          ctx.body = {
-            code: 200,
-            token: token
-          }
-        } else {
-          // 用户名 密码验证失败，返回提示
-          ctx.body = {
-            code: 404,
-            msg: '用户名或者密码错误'
-          }
-        }
-      } else {
-        // 图片验证码校验失败
-        ctx.body = {
-          code: 401,
-          msg: '图片验证码不正确,请检查！'
-        }
+    // 返回token
+    const { body } = ctx.request;
+    let sid = body.sid;
+    let code = body.code;
+    // 验证图片验证码的时效性、正确性
+    // let result = await checkCode(sid, code)
+    let result = true;
+    if (result) {
+      //验证用户账号密码是否正确
+      let checkUserPasswd = false;
+      let user = await User.findOne({ username: body.username });
+      if (user.password === body.password) {
+        checkUserPasswd = true;
       }
+      // mongoDB查库
+      if (checkUserPasswd) {
+        // 验证通过，返回Token数据
+        const userObj = user.toJSON();
+        const arr = ["password", "username"];
+        arr.map((item) => {
+          delete userObj[item];
+        });
+        let token = jsonwebtoken.sign({ _id: "brian" }, config.JWT_SECRET, {
+          expiresIn: "1d",
+        });
+        ctx.body = {
+          code: 200,
+          data: userObj,
+          token: token,
+        };
+      } else {
+        // 用户名 密码验证失败，返回提示
+        ctx.body = {
+          code: 404,
+          msg: "用户名或者密码错误",
+        };
+      }
+    } else {
+      // 图片验证码校验失败
+      ctx.body = {
+        code: 401,
+        msg: "图片验证码不正确,请检查！",
+      };
     }
   }
+}
 
 export default new loginController();
